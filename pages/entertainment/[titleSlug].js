cee40ccdfe319cdcc7006c26192325904a35dda8
Real Mohsin <realmohsin@outlook.com>
2021-03-14 00:19:52 -0500

    contentful data pulling working

---
+++ pages/entertainment/[titleSlug].js
@@ -0,0 +1,64 @@
+import Head from 'next/head'
+import {
+  getContentfulEntryById,
+  getEntryByTitleSlug,
+  getAllEntriesByContentType
+} from '../../lib/contentful'
+
+export async function getStaticPaths () {
+  const allInnerPageEntries = await getAllEntriesByContentType(
+    'romanoInnerPage'
+  )
+
+  const paths = allInnerPageEntries.items
+    .filter(({ fields: { slug } }) => slug.startsWith('entertainment'))
+    .map(({ fields: { slug } }) => ({
+      params: {
+        titleSlug: slug.replace('entertainment/', '')
+      }
+    }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps ({ params }) {
+  const entry = await getEntryByTitleSlug(
+    'romanoInnerPage',
+    `entertainment/${params.titleSlug}`
+  )
+  console.log('entry: ', entry)
+  return {
+    props: {
+      titleSlug: params.titleSlug,
+      ...entry.fields
+    }
+  }
+}
+
+export default function Home ({
+  titleSlug,
+  slug,
+  heroImage,
+  title,
+  mainBlurb,
+  pageContent
+}) {
+  console.log(heroImage)
+  return (
+    <div>
+      <Head>
+        <title>Romana Law</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <h1>{title}</h1>
+        <p>{slug}</p>
+        <img src={heroImage.fields.file.url} />
+      </main>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,27 @@ import {
   getAllEntriesByContentType
 } from '../lib/contentful'
 
-export async function getStaticProps () {
-  // const contentfulEntry = await getContentfulEntryById('4E5RmU2Xmzz8VGO68T3l4p')
-  const allInnerPageEntries = await getAllEntriesByContentType(
-    'romanoInnerPage'
-  )
-  return {
-    props: {
-      allInnerPageEntries
-    }
-  }
-}
+// export async function getStaticProps () {
+//   // const contentfulEntry = await getContentfulEntryById('4E5RmU2Xmzz8VGO68T3l4p')
+//   const allInnerPageEntries = await getAllEntriesByContentType(
+//     'romanoInnerPage'
+//   )
+//   return {
+//     props: {
+//       allInnerPageEntries
+//     }
+//   }
+// }
 
-export default function Home ({ allInnerPageEntries }) {
-  console.log(allInnerPageEntries)
+export default function Home () {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Romana Law</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>hello world</main>
+      <main>romano law</main>
     </div>
   )
 }
